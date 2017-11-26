724a62ea0e7de491011e37c123b3437eee313208
Renan Vasconcelos <renan.vasconcelos@orama.com.br>
2017-11-26 17:52:02 -0200

    added support for exchanging data frames

--- server.py
+++ server.py
@@ -1,14 +1,115 @@
 #!/usr/bin/env python
 from base64 import b64encode
 from hashlib import sha1
-import re, socket, threading, time
+import array
+import os
+import re
+import six
+import socket
+import struct
+import threading
 
 
 SERVER_HOST_PROTOCOL = "http"
-SERVR_HOST = "localhost"
+SERVER_HOST = "localhost"
 SERVER_PORT = 8000
 WEBSOCKET_ENDPOINT = "/ws"
 GUID = "398YTWA5-U832-85LO-72GZ-K2PM0FV64N80"
+OPCODE_TEXT = 0x1
+OPCODE_CLOSE = 0x8
+
+
+def _mask(mask_key, data):
+    """Apply mask to data based on mask_key."""
+    for i in range(len(data)):
+        data[i] ^= mask_key[i % 4]
+
+    if six.PY3:
+        return data.tobytes()
+    else:
+        return data.tostring()
+
+
+def get_masked(data):
+    """Return masked data for websocket frame."""
+    mask_key = os.urandom(4)
+    if data is None:
+        data = ""
+
+    bin_mask_key = mask_key
+    if isinstance(mask_key, six.text_type):
+        bin_mask_key = six.b(mask_key)
+
+    if isinstance(data, six.text_type):
+        data = six.b(data)
+
+    _mask_array = array.array("B", bin_mask_key)
+    _data_array = array.array("B", data)
+    masked_data = _mask(_mask_array, _data_array)
+
+    if isinstance(mask_key, six.text_type):
+        mask_key = mask_key.encode('utf-8')
+    return mask_key + masked_data
+
+
+def get_encoded_frame(data="", opcode=OPCODE_TEXT, mask=1):
+    """Return encoded frame."""
+    if opcode == OPCODE_TEXT and isinstance(data, six.text_type):
+        data = data.encode('utf-8')
+
+    length = len(data)
+    fin, rsv1, rsv2, rsv3, opcode = 1, 0, 0, 0, opcode
+
+    frame_header = chr(fin << 7 | rsv1 << 6 | rsv2 << 5 | rsv3 << 4 | opcode)
+
+    if length < 0x7e:
+        frame_header += chr(mask << 7 | length)
+        frame_header = six.b(frame_header)
+    elif length < 1 << 16:
+        frame_header += chr(mask << 7 | 0x7e)
+        frame_header = six.b(frame_header)
+        frame_header += struct.pack("!H", length)
+    else:
+        frame_header += chr(mask << 7 | 0x7f)
+        frame_header = six.b(frame_header)
+        frame_header += struct.pack("!Q", length)
+
+    if not mask:
+        return frame_header + data
+    return frame_header + get_masked(data)
+
+
+def is_close_frame(frame):
+    """Return True if frame has close connection opcode."""
+    frame_byte_array = [ord(character) for character in frame]
+    if frame_byte_array[0] == 136:
+        return True
+    return False
+
+
+def get_decoded_frame(frame):
+    """Return decoded frame data."""
+    frame_byte_array = [ord(character) for character in frame]
+    frame_length = frame_byte_array[1] & 127
+    first_mask_index = 2
+
+    if frame_length == 126:
+        first_mask_index = 4
+    elif frame_length == 127:
+        first_mask_index = 10
+
+    masks = [m for m in frame_byte_array[first_mask_index:first_mask_index + 4]]
+
+    first_data_byte_index = first_mask_index + 4
+    decoded_data = []
+    i = first_data_byte_index
+    j = 0
+    while i < len(frame_byte_array):
+        decoded_data.append(chr(frame_byte_array[i] ^ masks[j % 4]))
+        i += 1
+        j += 1
+
+    return decoded_data
 
 
 def get_response_general(is_websocket=False, is_method_not_allowed=False, is_bad_request=False):
@@ -30,21 +131,13 @@ def get_response_general(is_websocket=False, is_method_not_allowed=False, is_bad
         response_status = '200'
         response_status_text = 'OK'
 
-    response_general = '%s %s %s' % (response_proto, response_status, \
-                                     response_status_text)
+    response_general = '%s %s %s' % (response_proto, response_status, response_status_text)
     return response_general
-        
 
-def get_response_headers(is_websocket=False, body="", key=None, is_method_not_allowed=False, is_bad_request=False):
+
+def get_response_headers(is_websocket=False, body="", key=None, is_method_not_allowed=False):
     """Return a string with proper response headers."""
-    if is_bad_request:
-        response_headers = {
-            'Content-Type': 'application/json; encoding=utf8',
-            'Content-Length': '%d' % len(body),
-            'Connection': 'close',
-            'Access-Control-Allow-Origin': '*',
-        }
-    elif is_method_not_allowed:
+    if is_method_not_allowed:
         response_headers = {
             'Content-Type': 'application/json; encoding=utf8',
             'Connection': 'keep-alive',
@@ -56,8 +149,11 @@ def get_response_headers(is_websocket=False, body="", key=None, is_method_not_al
             'Upgrade': 'WebSocket',
             'Connection': 'Upgrade',
             'Sec-WebSocket-Accept': key,
-            'WebSocket-Origin': '%s://%s:%s' % (SERVER_HOST_PROTOCOL, SERVR_HOST, str(SERVER_PORT)),
-            'WebSocket-Location': 'ws://%s:%s' % (SERVR_HOST, str(SERVER_PORT)),
+            'WebSocket-Origin': '%s://%s:%s' % (SERVER_HOST_PROTOCOL,
+                                                SERVER_HOST,
+                                                str(SERVER_PORT)),
+            'WebSocket-Location': 'ws://%s:%s' % (SERVER_HOST,
+                                                  str(SERVER_PORT)),
             'WebSocket-Protocol': 'sample',
         }
     else:
@@ -67,16 +163,24 @@ def get_response_headers(is_websocket=False, body="", key=None, is_method_not_al
             'Connection': 'close',
             'Access-Control-Allow-Origin': '*',
         }
-    response_headers = ''.join('%s: %s\n' % (k, v) for k, v in \
-                          response_headers.iteritems())
+    response_headers = ''.join('%s: %s\n' % (k, v) for k, v in
+                               response_headers.iteritems())
     return response_headers
 
 
-def handle(s):
+def handle(s, source_address):
     """Handle socket execution."""
     text = s.recv(1024)
 
-    if "GET" not in text:
+    response_general = None
+    response_headers = None
+    should_close_connection = True
+    if source_address in successful_handshake_clients:
+        should_close_connection = is_close_frame(text)
+        if should_close_connection:
+            successful_handshake_clients.remove(source_address)
+        body = "{'status': 'success'}"
+    elif "GET" not in text:
         body = ""
         response_general = get_response_general(is_method_not_allowed=True)
         response_headers = get_response_headers(is_method_not_allowed=True)
@@ -91,35 +195,40 @@ def handle(s):
             if websocket_key_pattern is not None:
                 key = (websocket_key_pattern.groups()[0].strip())
                 response_key = b64encode(sha1(key + GUID).digest())
-                body = "{'status': 'success'}"
+                body = "{'status': 'upgraded'}"
                 response_general = get_response_general(is_websocket=True)
                 response_headers = get_response_headers(is_websocket=True,
                                                         body=body,
                                                         key=response_key)
+                should_close_connection = False
+                successful_handshake_clients.append(source_address)
             else:
                 body = "{'detail': 'Missing Sec-WebSocket-Key header'}"
                 response_general = get_response_general(is_bad_request=True)
-                response_headers = get_response_headers(body=body,
-                                                        is_bad_request=True)
+                response_headers = get_response_headers(body=body)
         else:
-            body = "{'status': 'fail'}"
+            body = "{'status': 'http'}"
             response_general = get_response_general(is_websocket=False)
-            response_headers = response_headers = get_response_headers(is_websocket=False,
-                                                                       body=body)
+            response_headers = get_response_headers(is_websocket=False,
+                                                    body=body)
 
-    s.send(response_general )
-    s.send('\n')
-    s.send(response_headers)
-    s.send('\n')
+    if response_general:
+        s.send(response_general)
+        s.send('\n')
+    if response_headers:
+        s.send(response_headers)
+        s.send('\n')
     s.send(body)
-    s.close()
+    if should_close_connection:
+        s.close()
 
 
 if __name__ == '__main__':
+    successful_handshake_clients = []
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('', SERVER_PORT));
-    s.listen(1);
+    s.bind(('', SERVER_PORT))
+    s.listen(1)
     while 1:
-      t,_ = s.accept();
-      threading.Thread(target = handle, args = (t,)).start()
+        t, source_address = s.accept()
+        threading.Thread(target=handle, args=(t, source_address[0])).start()
--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
-from server import SERVR_HOST, SERVER_PORT, WEBSOCKET_ENDPOINT
+from server import SERVER_HOST, SERVER_PORT, WEBSOCKET_ENDPOINT, get_encoded_frame, \
+    OPCODE_TEXT, OPCODE_CLOSE
 from unittest import TestCase
-import httplib, subprocess, time
+import httplib
+import socket
+import subprocess
+import time
+import unittest
 
 
 class TestClient(TestCase):
     def setUp(self):
-        self.connection_url = "%s:%s" % (SERVR_HOST, str(SERVER_PORT))
+        self.connection_url = "%s:%s" % (SERVER_HOST, str(SERVER_PORT))
         self.subprocess = subprocess.Popen("exec python ./server.py", stdout=subprocess.PIPE, shell=True)
         time.sleep(1)
 
@@ -28,22 +33,59 @@ class TestClient(TestCase):
         conn = httplib.HTTPConnection(self.connection_url)
         conn.request("GET", WEBSOCKET_ENDPOINT, headers=headers)
         response = conn.getresponse()
-        data = response.read()
+
         self.assertEqual(response.status, httplib.BAD_REQUEST)
-        self.assertIn("Missing Sec-WebSocket-Key header", data)
+        self.assertIn("Missing Sec-WebSocket-Key header", response.read())
 
         headers = {'Upgrade': 'WebSocket',
                    'Connection': 'Upgrade',
-                   'Sec-WebSocket-Key': '9aJYEbnLCWvczRQALUVpce=='}
+                   'Sec-WebSocket-Key': '9aJYEbnLCWvczRQALUVpce==',
+                   'Sec-WebSocket-Version': 13}
         conn = httplib.HTTPConnection(self.connection_url)
         conn.request("GET", WEBSOCKET_ENDPOINT, headers=headers)
         response = conn.getresponse()
+
         self.assertEqual(response.status, httplib.SWITCHING_PROTOCOLS)
         self.assertIn(('connection', 'Upgrade'), response.getheaders())
         self.assertIn(('upgrade', 'WebSocket'), response.getheaders())
 
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((socket.gethostbyname(SERVER_HOST), SERVER_PORT))
+
+        data = "Hello, world!"
+        sock.send(get_encoded_frame(data=data, opcode=OPCODE_TEXT))
+        self.assertEqual("{'status': 'success'}", sock.recv(1024))
+
+        sock.close()
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((socket.gethostbyname(SERVER_HOST), SERVER_PORT))
+
+        data = "Hello, again!"
+        sock.send(get_encoded_frame(data=data, opcode=OPCODE_TEXT))
+        self.assertEqual("{'status': 'success'}", sock.recv(1024))
+        sock.close()
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((socket.gethostbyname(SERVER_HOST), SERVER_PORT))
+
+        data = "Bye!"
+        sock.sendall(get_encoded_frame(data=data, opcode=OPCODE_CLOSE))
+        self.assertEqual("{'status': 'success'}", sock.recv(1024))
+        sock.close()
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((socket.gethostbyname(SERVER_HOST), SERVER_PORT))
+
+        data = "Hello, again!"
+        sock.send(get_encoded_frame(data=data, opcode=OPCODE_TEXT))
+        self.assertIn("HTTP/1.1 405 Method Not Allowed", sock.recv(1024))
+
+        sock.close()
+
     def tearDown(self):
         self.subprocess.kill()
+
 
 if __name__ == '__main__':
     unittest.main()
